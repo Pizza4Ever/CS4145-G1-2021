e0a6f7a9b8a02cdb5a575441b9e8096191d4634d
RNaomiGlans <rnglans@gmail.com>
2021-06-07 11:20:46 +0200

    Cleanup computervision.py

--- computervision.py
+++ computervision.py
@@ -1,5 +1,3 @@
-
-
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -21,64 +19,65 @@ computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredenti
 
 images_folder = os.path.join (os.path.dirname(os.path.abspath(__file__)), "static")
 
-# Open local image file
-local_image_path = os.path.join (images_folder, "14.jpg")
-local_image = open(local_image_path, "rb")
-
 # Call API
-description_result = computervision_client.describe_image_in_stream(local_image)
+def get_image_analysis(image):
 
-# Get the captions (descriptions) from the response, with confidence level
-print("Description of local image: ")
-if (len(description_result.captions) == 0):
-    print("No description detected.")
-else:
-    for caption in description_result.captions:
-        print("'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100))
+    description = get_image_description(image)
+    tags = get_image_tags(image)
 
-print("===== Categorize an Image - local =====")
-# Open local image file
-local_image = open(local_image_path, "rb")
-# Select visual feature type(s)
-local_image_features = ["categories"]
-# Call API
-categorize_results_local = computervision_client.analyze_image_in_stream(local_image, local_image_features)
-
-# Print category results with confidence score
-print("Categories from local image: ")
-if (len(categorize_results_local.categories) == 0):
-    print("No categories detected.")
-else:
-    for category in categorize_results_local.categories:
-        print("'{}' with confidence {:.2f}%".format(category.name, category.score * 100))
-
-print("===== Tag an Image - local =====")
-# Open local image file
-local_image = open(local_image_path, "rb")
-# Call API local image
-tags_result_local = computervision_client.tag_image_in_stream(local_image)
-
-# Print results with confidence score
-print("Tags in the local image: ")
-if (len(tags_result_local.tags) == 0):
-    print("No tags detected.")
-else:
-    for tag in tags_result_local.tags:
-        print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+    return description, tags
 
 
-print("===== Detect Objects - local =====")
-# Get local image with different objects in it
-local_image_objects = open(local_image_path, "rb")
-# Call API with local image
-detect_objects_results_local = computervision_client.detect_objects_in_stream(local_image_objects)
+def get_image_description(image):
+    local_image_path = os.path.join (images_folder, image)
+    local_image = open(local_image_path, "rb")
+    description_result = computervision_client.describe_image_in_stream(local_image)
+    local_image.close
 
-# Print results of detection with bounding boxes
-print("Detecting objects in local image:")
-if len(detect_objects_results_local.objects) == 0:
-    print("No objects detected.")
-else:
-    for object in detect_objects_results_local.objects:
-        print("object at location {}, {}, {}, {}".format( \
-        object.rectangle.x, object.rectangle.x + object.rectangle.w, \
-        object.rectangle.y, object.rectangle.y + object.rectangle.h))
+    # Get the captions (descriptions) from the response, with confidence level
+    print("Description of local image: ")
+    # Open local image file
+    
+    res = "No description detected."
+    if (len(description_result.captions) == 0):
+        print(res)
+        return res
+    else:
+        
+        for caption in description_result.captions:
+            confidence = caption.confidence * 100
+            res = caption.text
+            print("'{}' with confidence {:.2f}%".format(res, confidence))
+            # We only need the first (often the only) description
+            return res
+
+
+def get_image_tags(image):
+    print("===== Tag an Image - local =====")
+    # Open local image file
+    local_image_path = os.path.join (images_folder, image)
+    local_image = open(local_image_path, "rb")
+    # Call API local image
+    tags_result_local = computervision_client.tag_image_in_stream(local_image)
+    local_image.close
+
+    # Print results with confidence score
+    print("Tags in the local image: ")
+    res = "No tags detected."
+    if (len(tags_result_local.tags) == 0):
+        print(res)
+        return res
+    else:
+        res = ""
+        for tag in tags_result_local.tags:
+            confidence = tag.confidence * 100
+            if confidence > 80:
+                print("'{}' with confidence {:.2f}%".format(tag.name, tag.confidence * 100))
+                res += tag.name + ","
+
+        res = res[:-1]
+        return res
+
+
+if __name__ == "__main__":
+    get_image_analysis("18.jpg")
