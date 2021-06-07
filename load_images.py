249dd4bb83b3c382c5f250c2cd293b0a5687c5d8
RNaomiGlans <rnglans@gmail.com>
2021-06-07 14:34:25 +0200

    Include computer vision info when loading images in db

--- load_images.py
+++ load_images.py
@@ -1,4 +1,5 @@
 from os import walk
+import time
 import sqlite3
 import computervision as cv
 
@@ -10,14 +11,24 @@ _, _, filenames = next(walk("./static"))
 
 print(filenames)
 index = 0
+batch_size = 0
 for name in filenames:
-    description, tags = cv.get_image_description(name)
-
-
+    print("Current file:" + name)
+    batch_size +=1
+    description, tags = cv.get_image_analysis(name)
     cur.execute('''
                 INSERT INTO images VALUES ('%s', %d, '%s', '%s');
                 ''' % (name, index, description, tags))
     index += 1
+
+
+    # Currently used Azure account is a student account with a Free tier Computer Vision sevice.
+    # This means that only 10 API calls can be done per minute
+    if batch_size == 10:
+        print("Limit reached, sleeping for 1 min")
+        time.sleep(60)
+        batch_size = 0
+
 
 con.commit()
 
