21eb1ee1352af08ea8715c5254e6bf152aafafeb
RNaomiGlans <rnglans@gmail.com>
2021-06-13 00:22:44 +0200

    Add object for processing question 2 submissions

--- fetch_results_question2.py
+++ fetch_results_question2.py
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+=======
+from os import curdir, terminal_size
+>>>>>>> 75dfc6d... Add object for processing question 2 submissions
 from question_2_submission import Question_2_Submission
 import time
 from functools import reduce
@@ -17,20 +21,35 @@ toloka_client = toloka.TolokaClient(token, 'PRODUCTION')
 requester = toloka_client.get_requester()
 print(f'Your account: {requester}')
 
+con = sqlite3.connect('db.db')
+cur = con.cursor()
 
-# Function from toloka-kit github for retrieving results
+
+# Function from toloka-kit github for retrieving results adjusted for our case
 results_list = []
+
+
+def get_res_mult_queries(queries):
+    res = []
+    for query in queries:
+        cur.execute(query)
+        res.append(cur.fetchone()[0])
+
+    return res
+
 
 for assignment in toloka_client.get_assignments(pool_id=pool_id, status=toloka.assignment.Assignment.ACCEPTED):
     for task, solution in zip(assignment.tasks, assignment.solutions):
         images = task.input_values['image_id']
+        hints = task.input_values['hints']
+
         submission = Question_2_Submission()
         submission.correct_image = task.input_values["correct_image"]
-        image_index = images.index(submission.correct_image)
+        image_index = str(images.index(submission.correct_image))
 
         for i, round in enumerate(solution.output_values['result']):
             # Check if the correct image is eliminated
-            if int(image_index) in round:
+            if image_index in round:
                 submission.image_found = False
 
                 # If the correct image is eliminated when the honeypot hint is asked, this is an invalid submission
@@ -42,22 +61,28 @@ for assignment in toloka_client.get_assignments(pool_id=pool_id, status=toloka.a
                 else:
                     submission.honeypot_hint = "not applicable"
 
-            submission.rounds.append(",".join([images[int(i)] for i in round]))
+            
+            images_ids_queries = submission.get_queries_images_ids(round, images)
+            submission.rounds.append(get_res_mult_queries(images_ids_queries))
+        submission.assert_invalid_elimination_pattern()
 
-        results_list.append({
-            'assignment_id': assignment.id,
-            'input_values': task.input_values,
-            'output_values': solution.output_values
-        }) 
+        cur.execute(submission.get_image_id_query())
+        submission.correct_image_id = cur.fetchone()[0]
+        hints_ids_queries = submission.get_queries_hints_ids(hints)
+        submission.hints_ids = get_res_mult_queries(hints_ids_queries)
+        
+        results_list.append(submission)
+
+        # results_list.append({
+        #     'assignment_id': assignment.id,
+        #     'input_values': task.input_values,
+        #     'output_values': solution.output_values
+        # }) 
 
 print(f'New results received: {len(results_list)}' if len(results_list) > 0 else 'Not received any new results yet, try to run this cell later.')
 
 print(results_list[0])
 
 
-def get_hints_ids_query(hints):
-    pass
-
-
-def build_query():
+def build_insert_query():
     pass
---
+++ question_2_submission.py
@@ -0,0 +1,52 @@
+class Question_2_Submission:
+    images_per_game = 23
+
+    def __init__(self) -> None:
+        self.image_found = True
+        self.honeypot_hint = 'passed'
+        self.invalid = False
+        self.correct_image = None
+        self.rounds = []
+        self.completion_time= None
+        self.correct_image_id = None
+        self.hints_ids = []
+
+
+    def assert_invalid_elimination_pattern(self) -> bool:
+        empty_counter = 0
+
+        # If the first 3 rounds no images were eliminated, they just wanted to achieve the minimum
+        # amount of hints needed before submitting 
+        for i in range(0,4):
+            if self.rounds[i] == "":
+                empty_counter += 1
+
+        # If all eliminated images were selected in the 4th round they just wanted to submit asap
+        if empty_counter == 3 & len(self.rounds[3]) == self.images_per_game:
+            self.invalid = True
+
+        
+        return self.invalid
+
+
+
+    def get_image_id_query(self):
+        return f"SELECT image_id FROM images WHERE path='{self.correct_image}'"
+
+
+    def get_queries_hints_ids(self, hints):
+        queries = []
+        for hint in hints:
+            queries.append(f"SELECT hint_id FROM hints WHERE image_id={self.correct_image_id} AND hint='{hint}'")
+        
+        return queries
+
+
+    def get_queries_images_ids(self, round, image_paths):
+        queries = []
+        for image_i in round:
+            queries.append(f"SELECT image_id FROM images WHERE path='{image_paths[int(image_i)]}'")
+        
+        return queries
+
+        
